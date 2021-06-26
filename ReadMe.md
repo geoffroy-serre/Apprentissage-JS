2a15f7bc463ad4eaa8ffde0a7d74d44d0e4c4f75
Geoffroy Serre <geff1982@gmail.com>
2021-06-26 16:20:25 +0200

    fin du chapitre 1 de formation

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+Repo de ma formation Udemy sur le Javascript.
+
+Je m'efforcerais de faire un fichier texte avec le bilan de chaque chapitres.
+Cela me permet de tracker mes progrès, et aussi mon mind set sur ce langage.
+
--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,50 @@
-let currentResult;
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+let previousResult = 0;
+
+addBtn.addEventListener('click', addition);
+subtractBtn.addEventListener('click', substraction);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
+
+function getUserInput() {
+  return parseInt(userInput.value);
+}
+
+function resultTextAndOutput(operator, result, inputNumber) {
+  const logEntry = {
+    previous: previousResult,
+    operator: operator,
+    inputNumber: inputNumber,
+    result: currentResult,
+  };
+  logEntries.push(logEntry);
+  console.log('CURRENT OPERATION :' + logEntries[logEntries.length - 1]);
+  console.log('TOTAL OPERATIONS : ' + logEntries);
+  const descriptionCalc = `${result} ${operator} ${inputNumber}`;
+  outputResult(currentResult, logEntries);
+}
+
+function addition() {
+  previousResult = currentResult;
+  currentResult += getUserInput();
+  resultTextAndOutput('+', currentResult, getUserInput());
+}
+
+function substraction() {
+  previousResult = currentResult;
+  currentResult -= getUserInput();
+  resultTextAndOutput('-', currentResult, getUserInput());
+}
+
+function multiply() {
+  previousResult = currentResult;
+  currentResult *= getUserInput();
+  resultTextAndOutput('*', currentResult, getUserInput());
+}
+function divide() {
+  previousResult = currentResult;
+  currentResult /= getUserInput();
+  resultTextAndOutput('/', currentResult, getUserInput());
+}
